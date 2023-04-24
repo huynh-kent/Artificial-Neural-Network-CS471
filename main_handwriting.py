3e322fb1f887467b39864d5d2605db79c69af450
huynh-kent <huynhkentd@gmail.com>
2023-04-24 16:43:08 -0700

    strip space in layers

--- main_handwriting.py
+++ main_handwriting.py
@@ -337,7 +337,7 @@ if __name__ == '__main__':
     # load data
     df = get_df('A_Z_cleaned.csv', letter)
     # network layers
-    layers = str(network_layers).strip('')
+    layers = str(network_layers).strip(' ')
     # weights file path
     weights_file = f'weights_{letter}_{layers}.txt'
 
@@ -351,7 +351,7 @@ if __name__ == '__main__':
     #     print('------------------------------')
 
     # train
-    train(neural_network, df, lr = 0.1, n_epochs = 10, target_error = 0.05, n_batches=5,sample_size=10)
+    train(neural_network, df, lr = 0.1, n_epochs = 100, target_error = 0.05, n_batches=1,sample_size=10)
 
     # save trained weights
     save_weights(neural_network, weights_file)
